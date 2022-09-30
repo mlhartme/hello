7b6b75c4360fff1e252d73e09bfb9604224832c7
Michael Hartmeier <mlhartme.users.noreply@github.com>
2022-09-30 23:16:04 +0200

    simplify

--- src/docker/files/run.sh
+++ src/docker/files/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LANG=de_DE.UTF-8
-# invoke with exec to have proper kill signal handling
+# invoke with exec to run java as pid 1 and thus properly receive signals
 exec java -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -jar *.jar
